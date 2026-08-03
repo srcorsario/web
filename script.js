66b201f2190fd7e4999ba42411a1c421093109bb
Sr Corsario <116904782+srcorsario@users.noreply.github.com>
2026-08-03 12:56:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT9rPlxpax2lE0rN97c6Hoy_OxUwREqRb48juEBr9C91ZFY2UvaKgC8JdiRcwDrtBErXFVmFRh0Zr5e/pub?gid=0&single=true&output=csv';
 // NUEVO: Se registra la URL actualizada del App Script para las peticiones de sincronización del sistema
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxAlJLdzYXL_SdbDrUNJ2FZ6AiSA6_3e3trc3nMDA58AlVFHKlwXAL80ooi9jpQVjqClg/exec';
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxwoTzvZlyXr7AbkoGnLLFYMEtD5mqSrfHH-PjsmGTq7V2PWCsmL6V8hNcCWWxmTJc4zw/exec';
 const APP_VERSION = 'v3.0.6'; 
 
 // MODIFICADO: Agregado Coreano (KO) con su respectivo emoji compatible
